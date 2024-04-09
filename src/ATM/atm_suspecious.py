12e3ac0250b1b4551bc7bb80d89df98398553273
muhammadosamahq <muhammadosama.hq@gmail.com>
2024-04-09 15:49:45 +0500

    resolved all the requested issues

--- src/ATM/atm_suspecious.py
+++ src/ATM/atm_suspecious.py
@@ -11,86 +11,96 @@ import numpy as np
 import requests
 
 def suspecious_cases(results) -> dict[str: Any]:
-    try:
-        helmet_detected: bool = False
-        num_persons_flag: bool = False
-        class_ids: np.ndarray = results.boxes.cls.numpy()
-        num_persons: int = (class_ids == 7).sum()  # 7 is person id
-        if num_persons > 2:
-            num_persons_flag: bool = True
-        for result in results.boxes.data.tolist():
-            _, _, _, _, _, class_id = result
-            if results.names[int(class_id)].lower() == 'helmet':
-                helmet_detected: bool = True
+    if results is None: raise TypeError("results can not be empty")
+    helmet_detected: bool = False
+    num_persons_flag: bool = False
+    class_ids: np.ndarray = results.boxes.cls.numpy()
+    num_persons: int = (class_ids == 7).sum()  # 7 is person id
+    if num_persons > 2:
+        num_persons_flag: bool = True
+    for result in results.boxes.data.tolist():
+        _, _, _, _, _, class_id = result
+        if results.names[int(class_id)].lower() == 'helmet':
+            helmet_detected: bool = True
+    return {"helmet_detected": helmet_detected, "num_persons": num_persons, "num_persons_flag": num_persons_flag}
 
-        return {"helmet_detected": helmet_detected, "num_persons": num_persons, "num_persons_flag": num_persons_flag}
-    except Exception as e:
-        logging.error(f"Error in suspecious_cases function: {e}")
-        raise
 
 def check_person_duration(num_persons: int, person_presence_start_time: datetime, elapsed_time: timedelta) -> tuple[datetime, timedelta]:
-    try:
-        if num_persons != 0 and person_presence_start_time is None:
-            start_time: datetime = datetime.now()
-            return start_time, None
-        elif num_persons == 0 and person_presence_start_time is not None:
-            elapsed_time = datetime.now() - person_presence_start_time
-            return None, elapsed_time
-        else:
-            return person_presence_start_time, elapsed_time
-    except Exception as e:
-        logging.error(f"Error in check_person_duration function: {e}")
-        raise
+    if num_persons < 0:
+        raise ValueError("num_persons must be non-negative")
+    if not isinstance(person_presence_start_time, datetime):
+        raise TypeError("person_presence_start_time must be a datetime object")
+    if not isinstance(elapsed_time, timedelta):
+        raise TypeError("elapsed_time must be a timedelta object")
+    
+    if num_persons != 0 and person_presence_start_time == datetime(1970, 1, 1, 0, 0, 0):
+        start_time: datetime = datetime.now()
+        return start_time, timedelta(0)
+    elif num_persons == 0 and person_presence_start_time != datetime(1970, 1, 1, 0, 0, 0):
+        elapsed_time = datetime.now() - person_presence_start_time
+        return datetime(1970, 1, 1, 0, 0, 0), elapsed_time
+    else:
+        return person_presence_start_time, elapsed_time
+
 
 def presence_threshold_flag(person_presence: datetime, wait_threshould: int, num_persons: int) -> bool:
-    try:
-        if num_persons != 0 and person_presence is not None:
-            wait_time: timedelta = datetime.now() - person_presence
-            if wait_time.total_seconds() >= wait_threshould:
-                return True
-        elif num_persons == 0 and person_presence is None:
-            return False
-        else:
-            return False
-    except Exception as e:
-        logging.error(f"Error in presence_threshold_flag function: {e}")
-        raise
+    if not isinstance(person_presence, datetime):
+        raise TypeError("person_presence must be a datetime object")
+    if not isinstance(wait_threshould, int):
+        raise TypeError("wait_threshould must be an integer")
+    if num_persons < 0:
+        raise ValueError("num_persons must be non-negative")
+
+    if num_persons != 0 and person_presence != datetime(1970, 1, 1, 0, 0, 0):
+        wait_time: timedelta = datetime.now() - person_presence
+        if wait_time.total_seconds() >= wait_threshould:
+            return True
+    elif num_persons == 0 and person_presence == datetime(1970, 1, 1, 0, 0, 0):
+        return False
+    else:
+        return False
 
 def atm_suspecious(results, person_presence_start_time: datetime, elapsed_time: timedelta, wait_threshould: int) -> tuple[int, timedelta, dict[str: bool], datetime, str]:
-    try:
-        time_exceeded_flag: bool = False
-        sus_activity: dict = suspecious_cases(results)
-        person_presence_start_time, elapsed_time = check_person_duration(sus_activity['num_persons'], person_presence_start_time, elapsed_time)
-        time_exceeded_flag: bool = presence_threshold_flag(person_presence_start_time, wait_threshould, num_persons=sus_activity['num_persons'])
-        persons: int = sus_activity['num_persons']
-        flags: dict[str: bool] = {'num_persons_flag': sus_activity['num_persons_flag'],
-                 'time_exceeded_flag': time_exceeded_flag,
-                 'helmet_detected_flag': sus_activity['helmet_detected']}
-        
-        suspicious: bool = any(flags.values())   
-        suspicious_label: str = 'SUSPICIOUS' if suspicious else 'NORMAL'
-        
-        return persons, elapsed_time, flags, person_presence_start_time, suspicious_label
-    except Exception as e:
-        logging.error(f"Error in atm_suspecious function: {e}")
-        raise
+    if results is None:
+        raise TypeError("results can not be empty")
+    if not isinstance(person_presence_start_time, datetime):
+        raise TypeError("person_presence_start_time must be a datetime object")
+    if not isinstance(elapsed_time, timedelta):
+        raise TypeError("elapsed_time must be a timedelta object")
+    if not isinstance(wait_threshould, int):
+        raise TypeError("wait_threshould must be integer")
+    
+    time_exceeded_flag: bool = False
+    sus_activity: dict = suspecious_cases(results)
+    person_presence_start_time, elapsed_time = check_person_duration(int(sus_activity['num_persons']), person_presence_start_time, elapsed_time)
+    time_exceeded_flag: bool = presence_threshold_flag(person_presence_start_time, wait_threshould, num_persons=sus_activity['num_persons'])
+    persons: int = sus_activity['num_persons']
+    flags: dict[str: bool] = {'num_persons_flag': sus_activity['num_persons_flag'],
+                'time_exceeded_flag': time_exceeded_flag,
+                'helmet_detected_flag': sus_activity['helmet_detected']}
+    
+    suspicious: bool = any(flags.values())   
+    suspicious_label: int = 1 if suspicious else 0  # 1 for SUSPECIOUS 0 for NORMAL
+    
+    return persons, elapsed_time, flags, person_presence_start_time, suspicious_label
 
-def post_sus_data(suspicious: str, previous_suspicious: str) -> int:
-    try:
-        status = 0
-        if suspicious == 'SUSPICIOUS' and previous_suspicious == 'NORMAL':
-            data = {
-                'status': suspicious,
-                'timestamp': formatted_timestamp  
-            }
-            #status = send_data_to_endpoint(data, SUSPICIOUS_TARGET_URL, JWT_TOKEN)
-            #print("sent suspicious data")
-            status = 200
 
-        return status
-    except Exception as e:
-        logging.error(f"Error in post_sus_data function: {e}")
-        raise
+def post_sus_data(suspicious: int, previous_suspicious: int) -> int:
+    if not isinstance(suspicious, int):
+        raise TypeError("suspicious must be an integer")
+    if not isinstance(previous_suspicious, int):
+        raise TypeError("previous_suspicious must be an integer")
+    status = 0
+    if suspicious == 1 and previous_suspicious == 0:
+        data = {
+            'status': suspicious,
+            'timestamp': formatted_timestamp  
+        }
+        #status = send_data_to_endpoint(data, SUSPICIOUS_TARGET_URL, JWT_TOKEN)
+        #print("sent suspicious data")
+        status = 200
+
+    return status
 
 def main() -> None:
     try:
@@ -100,10 +110,10 @@ def main() -> None:
         ATM_MODEL = os.getenv('ATM_MODEL')
         VIDEO_PATH = os.path.join(os.getenv('ATM_VIDEO_PATHS'), VIDEO_NAME)
 
-        person_presence_start_time: datetime = None
-        elapsed_time: timedelta = None 
-        suspicious: bool = False
-        previous_suspicious: str = None
+        person_presence_start_time: datetime = datetime(1970, 1, 1, 0, 0, 0)
+        elapsed_time: timedelta = timedelta(0)
+        suspicious: int = 0  # 1 for SUSPECIOUS 0 for NORMAL
+        previous_suspicious: int = 0
 
         atm_model: YOLO = load_model(ATM_MODEL)
         cap: cv2.VideoCapture = load_video(VIDEO_PATH)
@@ -121,7 +131,7 @@ def main() -> None:
             post_sus_data(suspicious, previous_suspicious)
             previous_suspicious = suspicious
 
-            if elapsed_time is not None:
+            if elapsed_time is not timedelta(0):
                 cv2.putText(frame, f"Presence elapsed time: {elapsed_time.total_seconds()}", (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 4)
 
             cv2.putText(frame, f"Helmet: {all_sus_flags['helmet_detected_flag']}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4)
